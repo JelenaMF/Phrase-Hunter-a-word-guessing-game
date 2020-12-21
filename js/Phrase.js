115c6f5e86b833153e896a23c36ed7fcf8a7f60d
Jelena Feliciano <jfel217@gmail.com>
2020-12-20 22:54:43 -0500

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,22 +1,10 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
-/* step 1 */
-//create a phrase class to handle the creation of phrases 
 
-/* step 2 */
-//declare the phrase class 
 class Phrase {
     constructor(phrase) {
         this.phrase = phrase.toLowerCase();
     }
 }
-    /*test code */
- /*step 3 */
-   /*create a constructor method inside class
-    *phrase class has one parameter 'phrase' 
-        **properties phrase class contains 
-        *phrase: this is the actual phrase the phrase object is representing. this property
-            should be set to the 'phrase' parameter but converted to all lower case 
-*/
-//test code in apps.js
+
