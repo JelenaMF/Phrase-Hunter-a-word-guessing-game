6d170eea8f5d43bf155640d2214445f7eba8cbda
Jelena Feliciano <jfel217@gmail.com>
2020-12-23 16:54:01 -0500

    Update Game.js
    realized what I did wrong with line 19.

--- js/Game.js
+++ js/Game.js
@@ -16,7 +16,7 @@
      class Game {
           constructor () {
                this.missed = 0;
-               this.phrases = createPhrase();
+               this.phrases = this.createPhrase();
                this.activePhrase = 'null';
           }
           createPhrase() {
