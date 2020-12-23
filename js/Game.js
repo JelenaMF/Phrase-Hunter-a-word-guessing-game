a73dd4e7ed9c3cb6cb83dae170f149488d55958e
Jelena Feliciano <jfel217@gmail.com>
2020-12-23 17:12:34 -0500

    app/Game.js update
    removed the notes for the 4th step added step 5  getRandomPhrase() method not completed just added.

--- js/Game.js
+++ js/Game.js
@@ -7,12 +7,6 @@
  * getting random phrases 
  * checking for a win 
  * and removing life counter*/
-/* step one */
-//create a game class methods for starting and ending the game 
-//handle interactions
-//get a random phrase 
-//check for wins 
-//remove life from the scoreboard.
      class Game {
           constructor () {
                this.missed = 0;
@@ -27,13 +21,8 @@
                               new Phrase('Show me the money')];
                return phrases;
           }
+          getRandomPhrase() {
+               
+          }
      }
-/* step 4
-     inside the Game class create method called 'createPhrase() that creates and returns 
-     an array of 5 new Phrase objects and then set the 'phrases' property to call that 
-     method.
 
-     ** don't forget to pass the actual string phrase that the Phrase object is representing
-     A string phrase should only include letters and spaces-- no numbers punctuation
-     or other special characters. 
-*/
--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@
  * performs basic DOM selection, add event listeners and resets the game
  * when it ends */
 //add a new instance of the game class 
-const game = new Game();
-const phrase = new Phrase('Life is like a box of chocolates.');
-game.phrases.forEach((phrase, index) => {
-    console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
-});
+// const game = new Game();
+// const phrase = new Phrase('Life is like a box of chocolates.');
+// game.phrases.forEach((phrase, index) => {
+//     console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+// });
 
 //console.log(game);
 //add event listener for the start button and onscreen keyboard buttons 
