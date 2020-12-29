016eb16c89ce0a8067d8467d0bced31ffe376af3
Jelena Feliciano <jfel217@gmail.com>
2020-12-29 15:18:39 -0500

    Update app.js
    uncommented the test codes to make sure everything was working properly for steps 1-4

--- js/app.js
+++ js/app.js
@@ -4,16 +4,16 @@
  * performs basic DOM selection, add event listeners and resets the game
  * when it ends */
 //add a new instance of the game class 
-// const game = new Game();
-// const phrase = new Phrase('Life is like a box of chocolates.');
-// game.phrases.forEach((phrase, index) => {
-//     console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
-// });
+const game = new Game();
+const phrase = new Phrase('Life is like a box of chocolates.');
+game.phrases.forEach((phrase, index) => {
+    console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+});
 
 //console.log(game);
 //add event listener for the start button and onscreen keyboard buttons 
 document.querySelector('#btn__reset').addEventListener('click', (e) => {
-    console.log('phrase');
+    console.log('start game button working!');
 
 });
 /* step 4 
