4b525c7daf0f531a03758c516a73eec674581563
Jelena Feliciano <jfel217@gmail.com>
2021-01-11 11:56:17 -0500

    Update Phrase.js
    creating notes as I go to understand the project a little better

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,5 +7,14 @@ class Phrase {
     constructor(phrase) {
         this.phrase = phrase.toLowerCase();
     }
+    addPhraseToDisplay() {
+        //add letter placeholders to the display when game starts
+        //each letter is represented by an empty box give each letter an <li> element
+            /* call <ul> in <div> with id 'phrase' 
+                append <li> with class name "hide letter" to ul 
+                if (letter === letter.charAt()) {
+                    give <li> class attribute "show letter"
+                }*/
+    }
 }
 
