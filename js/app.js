0a6932daa013fa099f02ec4ca8ecda18c2764682
Jelena Feliciano <jfel217@gmail.com>
2021-01-19 00:07:23 -0500

    Update app.js
    mainly added notes thinking about how to start this part of the project.

--- js/app.js
+++ js/app.js
@@ -4,13 +4,14 @@
  * performs basic DOM selection, add event listeners and resets the game
  * when it ends */
 //add a new instance of the game class 
-let game; 
-
+const game = new Game();
 //add event listener for the start button and onscreen keyboard buttons 
 document.querySelector('#btn__reset').addEventListener('click', (e) => {
-    game = new Game()
     game.startGame();
-    
+    //when game is completed the gameboard resets by clicking startbutton
+        //removes all li elements from the Phrase ul element 
+        //enables all of the onscreen keyboard buttons and update keys class to hide
+        //resets all the heart images in the score board to display all liveHeart. 
 });
 
 const keys = document.querySelectorAll('.key');
