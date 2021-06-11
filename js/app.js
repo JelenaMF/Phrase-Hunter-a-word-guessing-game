e8e14a5c73e80e829cfaede5b80e43cbdbfcace0
Jelena Feliciano <jfel217@gmail.com>
2021-06-10 22:14:06 -0400

    Fixed timer on NewGame
    When the game is reset so is the timer. also removed some commented out phrases

--- js/app.js
+++ js/app.js
@@ -5,54 +5,7 @@
  * when it ends */
 
 let game;
-// const phrases = [
-//     {
-//             phrase:'Look at the flowers',
-//             hint: `Carol's line from the Walking Dead.` //add hint property 
-//         }, 
-//         {
-//             phrase: 'protect the planet',
-//             hint: 'environment phrase'
-//         },
-//         {
-//             phrase: 'Speak of the devil',
-//             hint: `phrase said when speaking about someone who comes up shortly after`
-//         },
-//         {
-//             phrase: 'Cut to the chase',
-//             hint: 'Said when someone is rushing a story being told'
-//         },
-//         {
-//             phrase: 'Hold your horses',
-//             hint: 'A figure of speech telling someone to calm down'
-//         },
-//         {
-//             phrase:'Close but no cigar',
-//             hint: 'a saying when someone comes close in a win'
-//         },
-//         {
-//             phrase: 'I want to rock',
-//             hint: 'A famous rock song'
-//         },
-//         {
-//             phrase: 'Show me the money',
-//             hint: 'A saying regarding money'
-//         },
-//         {
-//             phrase: 'Break a leg',
-//             hint: 'A phrase with many meanings one being for luck'
-//         },
-//         {
-//             phrase:'cutting corners',
-//             hint: 'cheating or taking a short cut'
-//         },
-//         {
-//             phrase:'Hit the sack',
-//             hint: 'A bedtime saying'
-//         }
-    
-// ]
-// console.log(phrases);
+
 const keys = document.querySelectorAll('.key');
 document.getElementById('timer').style.display = 'none';
 const startButton = document.querySelector('#btn__reset');
--- js/timer.js
+++ js/timer.js
@@ -17,7 +17,7 @@ const COLOR_CODES = {
   }
 };
 
-const TIME_LIMIT = 3;
+const TIME_LIMIT = 60;
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 let timerInterval = null;
@@ -51,6 +51,7 @@ function onTimesUp() {
   game.gameOver();
   if(timeLeft === 0){
     document.getElementById('timer').style.display = 'none';
+    timePassed = 0;
   } 
 }
 
